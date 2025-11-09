7f0796989fceb73af919113fbda4d0546b232a07
Case <90377400+CaseIRL@users.noreply.github.com>
2025-11-09 03:37:17 +0000

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,21 @@
+--[[
+##############################
+#  ____    _    ____  _____  #
+# / ___|  / \  / ___|| ____| #
+# | |    / _ \ \___ \|  _|   #
+# | |___/ ___ \ ___) | |___  #
+# \____/_/   \_|____/|_____| #            
+##############################
+]]
+
+fx_version "cerulean"
+games { "gta5" }
+
+name "fivem_zone_creator"
+version "1.0.0"
+description "A simple single file zone creator, made to be dropped into other resources."
+author "CaseIRL"
+repository 'https://github.com/caseirl/fivem_zone_creator'
+lua54 'yes'
+
+shared_script "zones.lua"
